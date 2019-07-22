970603b3e10215fa7564583e235a5d2c712b557f
Agustín Formoso <aformoso@ripe.net>
2019-07-22 12:03:47 +0200

    Stripping out unnecessary clutter

--- test-main.js
+++ test-main.js
@@ -17,25 +17,9 @@ require.config({
   baseUrl: '/base',
 
   paths: {
-    'jquery': "https://cdn.dev.lacnic.net/jquery-1.11.1.min",  // 1.11.1
-    "jquery-private": 'jquery-private',  //  'https://cdn.dev.lacnic.net/jquery-private',
-    "jquery-public": "jquery-1.6.2",  // 1.6.2
-    "jsonp": "https://cdn.dev.lacnic.net/jsonp",
-    "date-format": "https://cdn.dev.lacnic.net/date.format",
-    "simon": "https://rawgit.com/LACNIC/simon/master/simon-javascript/simon_probe_plugin",
-    "stun": "https://rawgit.com/LACNIC/natmeter/master/stun/app/static/app/js/stun",
-    "monitor": "https://rawgit.com/LACNIC/monitor/master/monitor/app/static/app/js/monitor"
   },
 
   map: {
-    // '*' means all modules will get 'jquery-private'
-    // for their 'jquery' dependency.
-    '*': { 'jquery': 'jquery-private' },
-
-    // 'jquery-private' wants the real jQuery module
-    // though. If this line was not here, there would
-    // be an unresolvable cyclic dependency.
-    'jquery-private': { 'jquery': 'jquery' }
   },
 
   // dynamically load all test files
